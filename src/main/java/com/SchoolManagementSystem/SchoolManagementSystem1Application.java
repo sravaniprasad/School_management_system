4315dffdc2105872d36a7a1a1c57939e34155457
sravaniprasad <99490552+sravaniprasad@users.noreply.github.com>
2024-03-22 11:51:48 +0530

    Update SchoolManagementSystem1Application.java

--- src/main/java/com/SchoolManagementSystem/SchoolManagementSystem1Application.java
+++ src/main/java/com/SchoolManagementSystem/SchoolManagementSystem1Application.java
@@ -2,10 +2,11 @@ package com.SchoolManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class SchoolManagementSystem1Application {
+public class SchoolManagementSystem1Application extends SpringBootServletIntializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagementSystem1Application.class, args);
